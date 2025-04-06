8ae00828bf68455ee287102dd1a7eab2ad7149be
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-04-06 06:57:15 +0300

    Add TOKEN_TYPE_CLAIM to SIMPLE_JWT settings for improved token handling

--- mindscribe/settings.py
+++ mindscribe/settings.py
@@ -86,6 +86,7 @@ SIMPLE_JWT = {
     'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
 
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
     
 }
 
